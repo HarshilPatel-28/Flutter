374d576ae537906d7d43e59b0990e5380606ef86
Harshil Patel <hp950036@gmail.com>
2024-06-13 15:20:12 +0530

    First Commit

---
+++ lib/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final String name = "Harshil";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+        backgroundColor: Colors.orange,
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcone $name..."),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
