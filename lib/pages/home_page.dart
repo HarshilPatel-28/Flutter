4b46765cbf19795787e078719c3e582edb48e9b0
Harshil Patel <hp950036@gmail.com>
2024-06-15 11:48:45 +0530

    Day-3 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,18 +3,20 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   final String name = "Harshil";
 
+  const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Catalog App"),
+        title: const Text("Catalog App"),
       ),
       body: Center(
         child: Container(
           child: Text("Welcone $name..."),
         ),
       ),
-      drawer: Drawer(),
+      drawer: const Drawer(),
     );
   }
 }
